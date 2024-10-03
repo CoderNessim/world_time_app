dd356fd4484122c9601ac2f220132ccbd46f7611
Nessim Yohros <nessim@MacBook-Pro-109.local>
2024-10-03 18:39:07 -0400

    made it so data can be passed to different routes

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -15,9 +15,10 @@ class _LoadingState extends State<Loading> {
     WorldTime instance = WorldTime(
         location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time
     });
   }
 
@@ -29,7 +30,7 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(padding: const EdgeInsets.all(50), child: Text(time)));
+    return const Scaffold(
+        body: Padding(padding: EdgeInsets.all(50), child: Text('loading')));
   }
 }
